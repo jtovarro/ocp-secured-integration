1b22be1be33a1f5c975e41742eb1a6b228582997
Alvaro Lopez Medina <alvarolop.medina@gmail.com>
2024-12-30 17:29:15 +0100

    🚚 Move to numbered files and folders for better readability

--- create_vault_secrets.sh
+++ create_vault_secrets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Vault configuration
-VAULT_ADDR="$(oc get route hashicorp-vault -n vault --template='https://{{.spec.host}}')"
+VAULT_ADDR="$(oc get route hashicorp-vault-server -n vault --template='https://{{.spec.host}}')"
 VAULT_TOKEN="root"                  # Default token in dev mode
 
 # Inline JSON definition of secrets
@@ -30,6 +30,7 @@ SECRETS_JSON='[
 ]'
 
 # Parse JSON and upload secrets
+echo -e "\nCreating dummy Secrets..."
 for row in $(echo "$SECRETS_JSON" | jq -c '.[]'); do
     path=$(echo "$row" | jq -r '.path')
     data=$(echo "$row" | jq -c '.data')
@@ -47,3 +48,72 @@ for row in $(echo "$SECRETS_JSON" | jq -c '.[]'); do
         echo "Failed to write secret to $path"
     fi
 done
+
+# Enable Kubernetes authentication
+echo -e "\nEnabling Kubernetes authentication..."
+curl -s \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data '{"type": "kubernetes"}' \
+    "$VAULT_ADDR/v1/sys/auth/kubernetes" > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Kubernetes authentication enabled"
+else
+    echo "Failed to enable Kubernetes authentication"
+fi
+
+
+# Configure the Kubernetes authentication method
+echo -e "\nConfiguring Kubernetes authentication..."
+KUBERNETES_SERVICE_HOST=172.30.0.1
+KUBERNETES_SERVICE_PORT=443
+curl -s \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data "{
+        \"kubernetes_host\": \"https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT\"
+    }" \
+    "$VAULT_ADDR/v1/auth/kubernetes/config" > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Kubernetes authentication configured"
+else
+    echo "Failed to configure Kubernetes authentication"
+fi
+
+
+# Create a policy for the role
+echo -e "\nCreating policy svc-policy..."
+curl -s \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request PUT \
+    --data '{
+        "policy": "path \"secret/data/*\" {\n  capabilities = [\"read\"]\n}"
+    }' \
+    "$VAULT_ADDR/v1/sys/policies/acl/svc-policy" > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Policy svc-policy created"
+else
+    echo "Failed to create policy svc-policy"
+fi
+
+# Create a role binding Kubernetes service account to the policy
+echo -e "\nCreating role webapp..."
+curl -s \
+    --header "X-Vault-Token: $VAULT_TOKEN" \
+    --request POST \
+    --data '{
+        "bound_service_account_names": ["*"],
+        "bound_service_account_namespaces": ["*"],
+        "policies": ["svc-policy"],
+        "ttl": "1h"
+    }' \
+    "$VAULT_ADDR/v1/auth/kubernetes/role/webapp" > /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Role webapp created"
+else
+    echo "Failed to create role webapp"
+fi
